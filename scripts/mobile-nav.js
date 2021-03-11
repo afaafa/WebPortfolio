e55350f77ea5edbc168b735a07021ea261399ee5
afaafa <afhazachra@gmail.com>
2021-03-11 16:36:35 +0700

    update sidebar

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -1,10 +1,17 @@
 const menu = document.querySelector("#menu")
 const drawer = document.querySelector("#drawer")
+const header = document.querySelector("header")
+const main = document.querySelector("main")
 
 menu.addEventListener("click", () => {
   if (drawer.style.display === "block") {
     drawer.style.display = "none"
+    header.classList.remove('transform')
+    main.classList.remove('transform')
   } else {
     drawer.style.display = "block"
+    header.classList.toggle('transform')
+    main.classList.toggle('transform')
   }
+
 })
